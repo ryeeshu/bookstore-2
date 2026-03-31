90917baeedd2e2f2310ab42a695f9e6b48776f90
R Yeeshu Dhurandhar <r.yeeshu@iitgn.ac.in>
2026-03-30 21:57:41 -0400

    solved + error

--- services/customer-service/src/main/java/edu/cmu/bookstore/controller/CustomerController.java
+++ services/customer-service/src/main/java/edu/cmu/bookstore/controller/CustomerController.java
@@ -1,12 +1,17 @@
 package edu.cmu.bookstore.controller;
 
+import edu.cmu.bookstore.exception.BadRequestException;
 import edu.cmu.bookstore.model.Customer;
 import edu.cmu.bookstore.model.request.CreateCustomerRequest;
 import edu.cmu.bookstore.service.CustomerService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/customers")
@@ -21,10 +26,8 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<Customer> createCustomer(@RequestBody CreateCustomerRequest request) {
         Customer createdCustomer = customerService.createCustomer(request);
-
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.LOCATION, "/customers/" + createdCustomer.getId());
-
         return new ResponseEntity<>(createdCustomer, headers, HttpStatus.CREATED);
     }
 
@@ -35,8 +38,33 @@ public class CustomerController {
     }
 
     @GetMapping
-    public ResponseEntity<Customer> getCustomerByUserId(@RequestParam("userId") String userId) {
+    public ResponseEntity<Customer> getCustomerByUserId(HttpServletRequest request) {
+        String rawQuery = request.getQueryString();
+
+        if (rawQuery == null || rawQuery.isBlank()) {
+            throw new BadRequestException("userId is required.");
+        }
+
+        String userId = extractUserId(rawQuery);
         Customer customer = customerService.getCustomerByUserId(userId);
         return ResponseEntity.ok(customer);
+    }
+
+    private String extractUserId(String rawQuery) {
+        for (String part : rawQuery.split("&")) {
+            int idx = part.indexOf('=');
+            if (idx <= 0) {
+                continue;
+            }
+
+            String key = part.substring(0, idx);
+            String value = part.substring(idx + 1);
+
+            if ("userId".equals(key)) {
+                return URLDecoder.decode(value, StandardCharsets.UTF_8);
+            }
+        }
+
+        throw new BadRequestException("userId is required.");
     }
 }
